1be2a44734e0093f31ebecbc565091aa45318824
unknown <wpp0725@163.com>
2022-07-03 12:23:55 +0800

    init

--- wppcmd.cmd
+++ wppcmd.cmd
@@ -1,11 +1,10 @@
 @echo off
 cd D:\DevOps\Projs\wpp_utils
-echo connect to github
+echo connect to github to update...
 git pull
 echo proj update finish
 set path=%PATH%;D:\DevOps\apache-maven-3.8.6\bin
-echo %path%
-echo hello %1
+echo hello param %1
 java -version
 pause
  
